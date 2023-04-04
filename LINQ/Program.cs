f9f3cc71c98545d2d52dd9458bcd36f76a7b400d
Robert Fassel <fcwrob@gmail.com>
2023-04-03 21:35:04 -0500

    Complete

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace LINQ
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>
+            {
+                "Goldeneye", "Zelda", "Super Mario Brothers", "Skyrim"
+            };
+            IEnumerable<string> gamesByLength =
+                videoGames.OrderBy(game => game.Length);
+
+            foreach(var item in gamesByLength)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
